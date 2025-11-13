52177ab9399bbb232082170f2bdcdd0bc35e0d95
Jeff Daly <jeffd@silicom-usa.com>
2025-11-12 22:53:04 -0500

    adl_n_cp_mec172x.c: Add Checkpoint fan event sense.
    Sense fan non-existence on power up sets LEDs.
    
    Signed-off-by: Jeff Daly <jeffd@silicom-usa.com>

--- app/fan_sense/fan_sense.h
+++ app/fan_sense/fan_sense.h
@@ -7,5 +7,9 @@
 #define __FAN_SENSE_H__
 
 void fan_sense_init(void);
+void fan1_sense_evt_processor(uint8_t evt);
+void fan2_sense_evt_processor(uint8_t evt);
+void fan3_sense_evt_processor(uint8_t evt);
+void fan4_sense_evt_processor(uint8_t evt);
 
 #endif /* __FAN_SENSE_H__ */
