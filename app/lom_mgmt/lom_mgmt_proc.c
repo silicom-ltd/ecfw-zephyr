d1afe9a144684e30cea056b81a4f9d68564386f1
blairw-brk <66346265+blairw-brk@users.noreply.github.com>
2025-12-26 14:10:29 +0800

    [LOM-EC] Fix wrong bit mask used for multiplier field
    1. multiplier is 6 bits now, bit mask should be 0x3f
    2. move sensor id calculation from fill_one_sensor() to
       lom_mgmt_dyn_sensor_table_init()
    
    Signed-off-by: blairw-brk <66346265+blairw-brk@users.noreply.github.com>

--- app/lom_mgmt/lom_mgmt_proc.c
+++ app/lom_mgmt/lom_mgmt_proc.c
@@ -52,93 +52,15 @@ struct hwmon_sram_entry_desc {
 #define SENS_INFO_MUL_BIT 0
 #define SENS_INFO_TYP_BIT 6 /* data type(2bits): 0: integer, 1: float */
 
-/*
-   The new added sensors always be stored in contiguous memory locations and at the end,
-   so the sensor_id could be calculated by sequence
-*/
-#if 0
-static const struct hwmon_sram_entry_desc hwmon_entries_mho150[] = {
-	/* hwmon_sdata[16]: 0x100 */
-	{  8/*0x100*/, hwmon_in,    6 }, /* P12V0A        */ /* 12V */
-	{  9/*0x120*/, hwmon_in,    7 }, /* P5V0A         */ /* 5V */
-	{ 10/*0x140*/, hwmon_in,    8 }, /* P3V3_ALW_ON   */ /* 3.3V Always On */
-	{ 11/*0x160*/, hwmon_in,    9 }, /* P1V8_ALW_ON   */ /* 1.8V Always On */
-	{ 12/*0x180*/, hwmon_temp,  1 }, /* AmbientTemp   */ /* Ambient */
-	{ 13/*0x1a0*/, hwmon_temp,  2 }, /* VR_Temp       */ /* Core VR */
-	{ 14/*0x1c0*/, hwmon_temp,  3 }, /* DDR_Temp      */ /* DDR */
-	{ 17/*0x220*/, hwmon_in,   10 }, /* P1V8A         */ /* 1.8V */
-	{ 18/*0x240*/, hwmon_temp,  4 }, /* CPU_Temp      */ /* CPU */
-	{ 19/*0x260*/, hwmon_in,   11 }, /* VCCIN_AUX     */ /* VCCINAUX */
-	{ 20/*0x280*/, hwmon_in,   12 }, /* 1.2V_VDD2     */ /* 1.2V_VDD2 */
-	//{ 21/*0x2a0*/, hwmon_in,   13 }, /* P0V95S        -*/
-	{ 22/*0x2c0*/, hwmon_in,   14 }, /* VTT_SODIMM    */ /* VTT_SODIMM */
-	{ 23/*0x2e0*/, hwmon_curr, 17 }, /* PWR_MON       */ /* Board Power */
-
-	/* hwmon_peci:      0x300 */
-	{ 24/*0x300*/, hwmon_temp,  5 }, /* CPU_PECI_Temp */ /* CPU PECI */
-
-	/* hwmon_fdata[4]:  0x320 */
-	//{ 25/*0x320*/, hwmon_fan, 15 }, /* Fan1_Speed    */
-	//{ 26/*0x340*/, hwmon_fan, 16 }, /* Fan2_Speed    */
-
-	/*
-	 * !! From here the Max sens_id is 17 !!
-	 */
-
-	/* hwmon.emc230x_fan, 8 entries*/
-	{ 33/*0x420*/, hwmon_fan,  18 }, /* Fan1_RPM    */
-	{ 34/*0x440*/, hwmon_fan,  00 }, /* Fan2_RPM    */
-	{ 35/*0x460*/, hwmon_fan,  00 }, /* Fan3_RPM    */
-	{ 36/*0x480*/, hwmon_fan,  00 }, /* Fan4_RPM    */
-	{ 37/*0x4a0*/, hwmon_fan,  00 }, /* Fan5_RPM    */
-	{ 38/*0x4c0*/, hwmon_fan,  00 }, /* Fan6_RPM    */
-	{ 39/*0x4e0*/, hwmon_fan,  00 }, /* Fan7_RPM    */
-	{ 40/*0x500*/, hwmon_fan,  00 }, /* Fan8_RPM    */
-
-	/* hwmon.sw_mon_thermal, 8 entries*/
-	{ 43/*0x560*/, hwmon_temp, 00 }, /* mfd3_temp     : 3.3V Left(id 26) */
-	{ 44/*0x580*/, hwmon_temp, 00 }, /* mfd4_temp     : 3.3V Right */
-	{ 45/*0x5a0*/, hwmon_temp, 00 }, /* mfd5_temp     : VDDH 1.1V */
-	{ 46/*0x5c0*/, hwmon_temp, 00 }, /* mfd6_temp_amb : PSU1 Left Ambient */
-	{ 47/*0x5e0*/, hwmon_temp, 00 }, /* mfd6_temp_hot : PSU1 Left Hot */
-	{ 48/*0x600*/, hwmon_temp, 00 }, /* mfd7_temp     : Main 3x Rails */
-	{ 49/*0x620*/, hwmon_temp, 00 }, /* mfd8_temp_amb : PSU2 Right Ambient */
-	{ 50/*0x640*/, hwmon_temp, 00 }, /* mfd8_temp_hot : PSU2 Right Hot*/
-	{ 51/*0x660*/, hwmon_temp, 00 }, /* switch_temp0  : Switch T0 */
-	{ 52/*0x680*/, hwmon_temp, 00 }, /* switch_temp1  : Switch T1 */
-
-	/* hwmon.sw_mon_voltage, 9 entries */
-	{ 53/*0x6a0*/, hwmon_in,   00 },  /* mfd3_vin     : 3.3V Left Vin (id 36) */
-	{ 54/*0x6c0*/, hwmon_in,   00 },  /* mfd3_vout    : 3.3V Left Vout */
-	{ 55/*0x6e0*/, hwmon_in,   00 },  /* mfd4_vin     : 3.3V Right Vin */
-	{ 56/*0x700*/, hwmon_in,   00 },  /* mfd4_vout    : 3.3V Right Vout */
-	{ 57/*0x720*/, hwmon_in,   00 },  /* mfd5_vin     : VDDH 1.1V Vin */
-	{ 58/*0x740*/, hwmon_in,   00 },  /* mfd5_vout    : VDDH 1.1V Vout */
-	{ 59/*0x760*/, hwmon_in,   00 },  /* mfd7_r0_vout : Core VDD */
-	{ 60/*0x780*/, hwmon_in,   00 },  /* mfd7_r1_vout : GOP VDD .8V */
-	{ 61/*0x7a0*/, hwmon_in,   00 },  /* mfd7_r2_vout : VDDA .9V */
-
-	/* hwmon.sw_mon_current, 5 entries */
-	{ 62/*0x7c0*/, hwmon_curr, 00 },  /* mfd3_iout    : 3.3V Left Iout (id 45) */
-	{ 63/*0x7e0*/, hwmon_curr, 00 },  /* mfd4_iout    : 3.3V Right Iout */
-	{ 64/*0x800*/, hwmon_curr, 00 },  /* mfd5_iout    : VDDH 1.1V Iout */
-	{ 65/*0x820*/, hwmon_curr, 00 },  /* mfd6_iout    : PSU1 Left Iout */
-	{ 66/*0x840*/, hwmon_curr, 00 },  /* mfd7_r0_iout : Core Iout */
-	{ 67/*0x860*/, hwmon_curr, 00 },  /* mfd7_r1_iout : GOP Iout */
-	{ 68/*0x880*/, hwmon_curr, 00 },  /* mfd7_r2_iout : VDDA Iout */
-	{ 69/*0x8a0*/, hwmon_curr, 00 },  /* mfd8_iout    : PSU2 Right Iout */
-
-	/* hwmon.sw_mon_power, 2 entries */
-	{ 70/*0x8c0*/, hwmon_power, 00 },  /* mfd6_pin    : PSU1 Left Pwr(id 54) */
-	{ 71/*0x8e0*/, hwmon_power, 00 },  /* mfd8_pin    : PSU2 Right Pwr */
-
-	{ 0xFFFF, 0, 0 }
-};
-#endif
+#define SENS_INFO_MUL_MASK 0x3F
 
 #define LOM_SENSOR_MAX 256
-#define DYN_SENSOR_ENTRY_IDX_BASE 43
-#define DYN_SENSOR_ID_BASE 18
+#define DYN_SENSOR_HWMON_IDX_BASE 43
+
+/*
+  The new added sensors always be stored in contiguous memory locations and at the end,
+  so the sensor_id could be calculated by sequence
+*/
 
 static struct hwmon_sram_entry_desc hwmon_entries[LOM_SENSOR_MAX] = {
 	/* hwmon_sdata[16]: 0x100 */
@@ -169,14 +91,14 @@ static struct hwmon_sram_entry_desc hwmon_entries[LOM_SENSOR_MAX] = {
 	 */
 
 	/* hwmon.emc230x_fan@33, 8 entries*/
-	{ 33/*0x420*/, hwmon_fan,  18 }, /* Fan1_RPM    */ /*new sens_id start from 18 */
-	{ 34/*0x440*/, hwmon_fan,  00 }, /* Fan2_RPM    */
-	{ 35/*0x460*/, hwmon_fan,  00 }, /* Fan3_RPM    */
-	{ 36/*0x480*/, hwmon_fan,  00 }, /* Fan4_RPM    */
-	{ 37/*0x4a0*/, hwmon_fan,  00 }, /* Fan5_RPM    */
-	{ 38/*0x4c0*/, hwmon_fan,  00 }, /* Fan6_RPM    */
-	{ 39/*0x4e0*/, hwmon_fan,  00 }, /* Fan7_RPM    */
-	{ 40/*0x500*/, hwmon_fan,  00 }, /* Fan8_RPM    */
+	{ 33/*0x420*/, hwmon_fan,  18 }, /* Fan1_RPM    */
+	{ 34/*0x440*/, hwmon_fan,  19 }, /* Fan2_RPM    */
+	{ 35/*0x460*/, hwmon_fan,  20 }, /* Fan3_RPM    */
+	{ 36/*0x480*/, hwmon_fan,  21 }, /* Fan4_RPM    */
+	{ 37/*0x4a0*/, hwmon_fan,  22 }, /* Fan5_RPM    */
+	{ 38/*0x4c0*/, hwmon_fan,  23 }, /* Fan6_RPM    */
+	{ 39/*0x4e0*/, hwmon_fan,  24 }, /* Fan7_RPM    */
+	{ 40/*0x500*/, hwmon_fan,  25 }, /* Fan8_RPM    */
 
 	{ 0xFFFF, 0, 0 },
 };
@@ -205,14 +127,16 @@ static struct lom_mgmt_task lom_mgmt_task;
 static void lom_mgmt_dyn_sensor_table_init(void)
 {
 	int start_idx;
-	int entry_idx = DYN_SENSOR_ENTRY_IDX_BASE;
+	int used_max_sens_id = 0;
+	int entry_idx = DYN_SENSOR_HWMON_IDX_BASE;
 	int i;
-
-	LOM_MGMT_DBG("Dynamic sensors items init");
 
 	for (i = 0; i < LOM_SENSOR_MAX; i++) {
 		if (hwmon_entries[i].entry_idx == 0xFFFF) {
 			break;
+		}
+		if (hwmon_entries[i].sens_id > used_max_sens_id) {
+			used_max_sens_id = hwmon_entries[i].sens_id;
 		}
 	}
 	start_idx = i;
@@ -220,7 +144,7 @@ static void lom_mgmt_dyn_sensor_table_init(void)
 	for (i = 0; i < SW_THERMAL_SENSOR_NUM; i++, entry_idx++) {
 		hwmon_entries[start_idx + i].entry_idx = entry_idx;
 		hwmon_entries[start_idx + i].sens_type = hwmon_temp;
-		hwmon_entries[start_idx + i].sens_id   = 0;
+		hwmon_entries[start_idx + i].sens_id   = ++used_max_sens_id;
 
 		LOM_MGMT_DBG("<TEMP> SENS[%02d]: { %d, %d, %3d }", start_idx +i,
 			hwmon_entries[start_idx + i].entry_idx,
@@ -232,7 +156,7 @@ static void lom_mgmt_dyn_sensor_table_init(void)
 	for (i = 0; i < SW_VOLTAGE_SENSOR_NUM; i++, entry_idx++) {
 		hwmon_entries[start_idx + i].entry_idx = entry_idx;
 		hwmon_entries[start_idx + i].sens_type = hwmon_in;
-		hwmon_entries[start_idx + i].sens_id   = 0;
+		hwmon_entries[start_idx + i].sens_id   = ++used_max_sens_id;
 
 		LOM_MGMT_DBG("<VOLT> SENS[%02d]: { %d, %d, %3d }", start_idx +i,
 			hwmon_entries[start_idx + i].entry_idx,
@@ -244,20 +168,19 @@ static void lom_mgmt_dyn_sensor_table_init(void)
 	for (i = 0; i < SW_CURRENT_SENSOR_NUM; i++, entry_idx++) {
 		hwmon_entries[start_idx + i].entry_idx = entry_idx;
 		hwmon_entries[start_idx + i].sens_type = hwmon_curr;
-		hwmon_entries[start_idx + i].sens_id   = 0;
+		hwmon_entries[start_idx + i].sens_id   = ++used_max_sens_id;
 
 		LOM_MGMT_DBG("<CURR> SENS[%02d]: { %d, %d, %3d }", start_idx +i,
 			hwmon_entries[start_idx + i].entry_idx,
 			hwmon_entries[start_idx + i].sens_type,
 			hwmon_entries[start_idx + i].sens_id);
-
 	}
 	start_idx += SW_CURRENT_SENSOR_NUM;
 
 	for (i = 0; i < SW_POWER_SENSOR_NUM; i++, entry_idx++) {
 		hwmon_entries[start_idx + i].entry_idx = entry_idx;
 		hwmon_entries[start_idx + i].sens_type = hwmon_power;
-		hwmon_entries[start_idx + i].sens_id   = 0;
+		hwmon_entries[start_idx + i].sens_id   = ++used_max_sens_id;
 
 		LOM_MGMT_DBG("<POWR> SENS[%02d]: { %d, %d, %3d }", start_idx +i,
 			hwmon_entries[start_idx + i].entry_idx,
@@ -268,8 +191,7 @@ static void lom_mgmt_dyn_sensor_table_init(void)
 
 	hwmon_entries[start_idx].entry_idx = 0xFFFF;
 
-	LOM_MGMT_DBG("<LAST> SENS[%02d]: { %d, %d, %3d }", start_idx,
-		hwmon_entries[start_idx].entry_idx,
+	LOM_MGMT_DBG("<LAST> SENS[%02d]: { -1, %d, %3d }", start_idx,
 		hwmon_entries[start_idx].sens_type,
 		hwmon_entries[start_idx].sens_id);
 }
@@ -302,27 +224,12 @@ static struct lom_mgmt_i2c_callbacks callbacks = {
 };
 
 static inline void fill_one_sensor(struct sensor_record *srd,
-	const struct hwmon_sram_entry_desc * ent, uint8_t *sens_id_max)
+	const struct hwmon_sram_entry_desc * ent)
 {
 	uint16_t *sram = (uint16_t *)hwmon_data;
 	uint16_t offset = (ent->entry_idx * 32) / 2; /* offset in two byte unit */
 
-	/* set sens_id */
-	uint8_t sens_id = ent->sens_id;
-	if (sens_id == 0) {
-		sens_id = ++(*sens_id_max);
-	}
-	else if (sens_id > *sens_id_max) {
-		*sens_id_max = sens_id;
-	}
-	else {
-		__ASSERT(sens_id != *sens_id_max, "Duplicated sens_id %d", sens_id);
-	}
-
-	uint8_t mul = 0;
-	if (ent->sens_type != hwmon_fan && ent->sens_type != hwmon_pwm) {
-		mul = (uint8_t)(sram[offset + 7] & 0x1); /* mul */
-	}
+	uint8_t mul = (uint8_t)(sram[offset + 7] & SENS_INFO_MUL_MASK); /* mul */
 
 	if (ent->sens_type == hwmon_fan) {
 		srd->info = (SENS_DTYPE_INTEG << SENS_INFO_TYP_BIT) | mul;
@@ -331,7 +238,7 @@ static inline void fill_one_sensor(struct sensor_record *srd,
 		srd->info = (SENS_DTYPE_FLOAT << SENS_INFO_TYP_BIT) | mul;
 	}
 
-	srd->sens_id = sens_id;
+	srd->sens_id = ent->sens_id;
 	srd->value = htons(sram[offset]);
 #ifdef SENSOR_DRY_RUN
 	if (ent->entry_idx >= 33) {
@@ -344,7 +251,6 @@ static int do_get_sensors(uint8_t *res_data, uint16_t *dat_size)
 {
 	struct sensor_record * srd;
 	uint16_t data_off = 0;
-	uint8_t sens_id_max = 0;
 
 	for (int i = 0; hwmon_entries[i].entry_idx != 0xFFFF; i++) {
 		if (data_off + SENSOR_RECORD_SIZE > RES_DLEN_MAX) {
@@ -353,7 +259,7 @@ static int do_get_sensors(uint8_t *res_data, uint16_t *dat_size)
 
 		srd = (struct sensor_record *)&(res_data[data_off]);
 
-		fill_one_sensor(srd, &hwmon_entries[i], &sens_id_max);
+		fill_one_sensor(srd, &hwmon_entries[i]);
 
 		LOM_MGMT_DBG(">> SENS@hwmon[%03d] 0x%02x %3d 0x%04x", hwmon_entries[i].entry_idx,
 			srd->info, srd->sens_id, ntohs(srd->value));
@@ -678,9 +584,9 @@ void lom_mgmt_thread(void *p1, void *p2, void *p3)
 	init_espi_event_monitor();
 	init_gpio_event_monitor();
 
-	lom_mgmt_dyn_sensor_table_init();
-
 	wait_hwdata_ready(normal_period);
+
+	lom_mgmt_dyn_sensor_table_init();
 
 	lom_mgmt_i2c_set_callbacks(lom_mgmt_dev, &callbacks);
 
