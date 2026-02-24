60fa4ecf2d5cc0ae85de6774de3a2509356aabfc
blairw-brk <blairw@silicom.co.il>
2026-02-24 14:27:12 +0800

    sw-sensors: simplify hwmon_cp.h
    Use DT_PROP_LEN_OR simplify the definition of SW sensor number macro
    
    Signed-off-by: blairw-brk <blairw@silicom.co.il>

--- drivers/hwmon_cp.h
+++ drivers/hwmon_cp.h
@@ -9,45 +9,20 @@
 
 #include <zephyr/device.h>
 
-#if !DT_NODE_HAS_PROP(DT_PATH(zephyr_user), sw_die_temp_sensors)
-#warn "Property sw_die_temp_sensors does not exist in zephyr,user node!"
-#define SW_DIE_THERMAL_SENSOR_NUM 0
-#else
-#define SW_DIE_THERMAL_SENSOR_NUM				\
-	DT_PROP_LEN(DT_PATH(zephyr_user), sw_die_temp_sensors)
-#endif
+#define SW_DIE_THERMAL_SENSOR_NUM					\
+	DT_PROP_LEN_OR(DT_PATH(zephyr_user), sw_die_temp_sensors, 0)
 
-#if !DT_NODE_HAS_PROP(DT_PATH(zephyr_user), sw_amb_temp_sensors)
-#warn "Property sw_amb_temp_sensors does not exist in zephyr,user node!"
-#define SW_AMB_THERMAL_SENSOR_NUM 0
-#else
-#define SW_AMB_THERMAL_SENSOR_NUM 				\
-	DT_PROP_LEN(DT_PATH(zephyr_user), sw_amb_temp_sensors)
-#endif
+#define SW_AMB_THERMAL_SENSOR_NUM					\
+	DT_PROP_LEN_OR(DT_PATH(zephyr_user), sw_amb_temp_sensors, 0)
 
-#if !DT_NODE_HAS_PROP(DT_PATH(zephyr_user), sw_volt_sensors)
-#warn "Property sw_volt_sensors does not exist in zephyr,user node!"
-#define SW_VOLTAGE_SENSOR_NUM 0
-#else
-#define SW_VOLTAGE_SENSOR_NUM (						\
-	DT_PROP_LEN(DT_PATH(zephyr_user), sw_volt_sensors))
-#endif
+#define SW_VOLTAGE_SENSOR_NUM 						\
+	DT_PROP_LEN_OR(DT_PATH(zephyr_user), sw_volt_sensors, 0)
 
-#if !DT_NODE_HAS_PROP(DT_PATH(zephyr_user), sw_curr_sensors)
-#warn "Property sw_curr_sensors does not exist in zephyr,user node!"
-#define SW_CURRENT_SENSOR_NUM 0
-#else
-#define SW_CURRENT_SENSOR_NUM (					\
-	DT_PROP_LEN(DT_PATH(zephyr_user), sw_curr_sensors))
-#endif
+#define SW_CURRENT_SENSOR_NUM						\
+	DT_PROP_LEN_OR(DT_PATH(zephyr_user), sw_curr_sensors, 0)
 
-#if !DT_NODE_HAS_PROP(DT_PATH(zephyr_user), sw_powr_sensors)
-#warn "Property sw_powr_sensors does not exist in zephyr,user node!"
-#define SW_POWER_SENSOR_NUM 0
-#else
-#define SW_POWER_SENSOR_NUM (					\
-	DT_PROP_LEN(DT_PATH(zephyr_user), sw_powr_sensors))
-#endif
+#define SW_POWER_SENSOR_NUM						\
+	DT_PROP_LEN_OR(DT_PATH(zephyr_user), sw_powr_sensors, 0)
 
 #define SW_THERMAL_SENSOR_NUM (SW_DIE_THERMAL_SENSOR_NUM + SW_AMB_THERMAL_SENSOR_NUM)
 
