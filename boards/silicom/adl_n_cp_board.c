cf0343000987f50545589005df245914c66ccca1
Basharat Martin <basharatm@silicom-usa.com>
2026-06-16 00:31:36 -0400

    [System IDs] : Added sbl version, board id and bom id
    Introduced set_sys_ids(), get_sbl_version(), get_board_id(),
    get_bom_id() functions,
    
    these functions can be used for future use, but for now only
    the bom_id is necessary to facilitate Maestro DDR5 VTT_SODIMM.
    
    Signed-off-by: Basharat Martin <basharatm@silicom-usa.com>

--- boards/silicom/adl_n_cp_board.c
+++ boards/silicom/adl_n_cp_board.c
@@ -20,8 +20,47 @@
 LOG_MODULE_REGISTER(board, CONFIG_BOARD_LOG_LEVEL);
 
 static uint16_t plat_data = 0x2400;
+static struct sys_ids g_sys_ids;
 
 uint16_t get_platform_id(void)
 {
 	return plat_data;
+}
+
+uint8_t get_bom_id (void)
+{
+    return g_sys_ids.bom_id;
+}
+
+uint8_t get_board_id (void)
+{
+    return g_sys_ids.board_id;
+}
+
+struct sbl_version *get_sbl_version(void)
+{
+    /*
+      major_version;
+      minor_version;
+      build_number_high;
+      build_number_low;
+      flags;
+        //flags: [3:0]reserved, [4]image_arch,
+        //       [5]bld_debug,  [6]fsp_debug,
+        //       [7]dirty
+    */
+
+    return &g_sys_ids.sbl;
+}
+
+void set_sys_ids(const uint8_t *pdata, uint8_t len)
+{
+    uint8_t i = 0;
+    uint8_t *ptr = (uint8_t *)&g_sys_ids;
+
+    while (i != len && i < 10)
+    {
+        ptr[i] = pdata[i];
+        i++;
+    }
 }
