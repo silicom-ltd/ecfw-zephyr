ace39398025b669a16d540e45b923b3f12de23e4
Basharat Martin <basharatm@silicom-usa.com>
2026-07-23 10:33:46 -0400

    [System IDs] : Add image_id to Sbl version - Maestro
    Added image_id to sbl version. This is an addition of 8 bytes
    of uint8 array to sbl version struct.
    
    get_sbl_version function provides the sbl version to LOM with
    the following members:
    
    Struct
    {
        image_id[8],
        major_version,
        minor_version,
        build_number_high,
        build_number_low,
        flags
        //flags:  [3:0]reserved, [4]image_arch,
        //        [5]bld_debug,  [6]fsp_debug,
        //        [7]dirty
    }
    
    Note: System IDs will have 15 bytes of data including bom_id
    and board_id. Currently, Maestro has no use of sbl versoin on
    the EC side.
    
    Signed-off-by: Basharat Martin <basharatm@silicom-usa.com>

--- boards/silicom/adl_n_cp_mec172x.h
+++ boards/silicom/adl_n_cp_mec172x.h
@@ -155,6 +155,7 @@ extern uint8_t platformskutype;
 
 // struct sbl version for LOM
 struct sbl_version {
+    uint8_t image_id[8];
     uint8_t major_version;
     uint8_t minor_version;
     uint8_t build_number_high;
@@ -167,7 +168,7 @@ struct sbl_version {
 
 /**
     struct sys_ids holds all the members of struct sbl_version
-    e.g major, minor, bld_number_high and bld_number_low, flags,
+    e.g image_id, major, minor, bld_number_high and bld_number_low, flags,
     and additionally board_id and bom_id
 **/
 struct sys_ids {
